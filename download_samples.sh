8e4cb88c7d078d17fcd4f03273ed09c6fadc7cea
yegor <prikaziuk@gmail.com>
2015-10-24 21:08:06 +0300

    Update

---
+++ download_samples.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This downloads whole series using tab-delimited format file
+
+SAMPLES=$1
+
+links=`awk -F "\t" '{ if (NR > 1)  print $31 }' $SAMPLES`
+
+for link in $links
+do
+    wget $link
+done
+
+awk -F "\t" '{ if (NR > 1)  print $30 }' $SAMPLES > errs.txt
+awk -F "\t" '{ if (NR > 1)  print $1 }' $SAMPLES > samps.txt
+paste errs.txt samps.txt > SAMPLES
+rm errs.txt samps.txt
--- kallisto_dirrun.sh
+++ kallisto_dirrun.sh
@@ -3,10 +3,10 @@
 
 # This runs kallisto quant for all fastq.gz files (must be run from directory with series)
 
-SPECIES=$2
-PAIRED=$3
+SPECIES=$1
+PAIRED=$2
 
-if [[ $PAIRED = "" || $SPECIES = "" || $READS = "" ]]; then
+if [[ $PAIRED = "" || $SPECIES = ""  ]]; then
     echo "usage kallisto_dirrun.sh vXX p/s"
 elif [ $PAIRED = "p" ]; then
     for FN in fastqs/*.R1.fastq.gz
@@ -17,7 +17,7 @@ elif [ $PAIRED = "p" ]; then
         echo $TAG
         gunzip -c fastqs/${TAG}.R1.fastq.gz > ${TAG}.R1.fastq
         gunzip -c fastqs/${TAG}.R2.fastq.gz > ${TAG}.R2.fastq
-        kallisto quant -i /media/DISK1/reference/Kallisto_indexes/gencode.${SPECIES}.index -o kallisto/${TAG} ${TAG}.R1.fastq ${TAG}.R2.fastq &> logs/${TAG}.kallisto.log
+        kallisto quant -i /media/DISK1/reference/Kallisto_indexes/gencode.${SPECIES}.index -o ${TAG}_kallisto_out ${TAG}.R1.fastq ${TAG}.R2.fastq
         rm ${TAG}.R1.fastq
         rm ${TAG}.R2.fastq &
     done
@@ -29,8 +29,8 @@ elif [ $PAIRED = "s" ]; then
         TMPTAG=${FN%%.fastq.gz}
         TAG=${TMPTAG#fastqs/}
         echo $TAG
-        gunzip -c $READS/$FN > ${TAG}.fastq
-        kallisto quant -i /media/DISK1/reference/Kallisto_indexes/gencode.${SPECIES}.index -o kallisto/${TAG}_kallisto_out --single ${TAG}.fastq &> logs/${TAG}.kallisto.log
+        gunzip -c fastqs/${TAG}.fastq.gz > ${TAG}.fastq
+        kallisto quant -i /media/DISK1/reference/Kallisto_indexes/gencode.${SPECIES}.index -o kallisto/${TAG}_kallisto_out --single ${TAG}.fastq -l 190 -s 10 &> kallisto_logs/${TAG}.kallisto.log
         rm ${TAG}.fastq &
     done
     wait
