4a6bcaf1bd6b3a38160e7773b9a1d0e73717dac5
Yury Barbitoff <barbitoff@bk.ru>
2015-10-27 17:45:40 +0300

    Only serious stuff

--- salmon_kallisto_cors.r
+++
@@ -1,8 +0,0 @@
-cors = c()
-for (exp in dir('./barbitoff/salmon/')){
-    sq = read.table(paste0('./barbitoff/salmon/', exp))
-    kq = read.table(paste0('./barbitoff/kallisto/', exp), header=T)
-    cors = c(cors, cor(sq[, 3], kq[, 5]))
-}
-names(cors) = dir('./barbitoff/salmon')
-print(cors)
